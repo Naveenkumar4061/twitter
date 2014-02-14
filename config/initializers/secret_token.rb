0cf398d4b9a6ac6aa790c930a3f602e98b2fb1c3
naveenkumar4061 <naveenkumar4061@gmail.com>
2014-02-14 09:55:59 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'd4f6a99d637ddd87ebcf0edd86a4062dfe9b65ac00aae4277bfd8230ad9fbeb52b62c685db7ea609b5d6c28d8bed9d5649841add41414262af23e773d1b0da5b'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Twitter::Application.routes.draw do
+  devise_for :users
+  root :to => 'home#index'
+end
